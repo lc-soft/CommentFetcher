28cb95daa1fed13175e0dd4e07db25f056ebd136
Liu Chao <lc-soft@live.cn>
2018-06-30 21:55:15 +0800

    refactor: merge token parameter

--- comment_fetcher/__init__.py
+++ comment_fetcher/__init__.py
@@ -9,11 +9,10 @@ class Comment(object):
             .format(self.start, self.end, len(self.content))
 
 class CommentScanner(object):
-    def __init__(self, token, line_token, debug=False):
+    def __init__(self, tokens, debug=False):
         self.buffer = []
-        self.token = token
-        self.line_token = line_token
-        self.is_multiple = False
+        self.tokens = tokens
+        self.picked_token = None
         self.picked_line = None
         self.debug = debug
 
@@ -32,22 +31,19 @@ class CommentScanner(object):
         print '%2d | %-80s | %s %s' % (i, line, mark, description)
 
     def scan_line_begin(self, line):
-        start = line.find(self.token[0])
-        line_start = line.find(self.line_token)
-        if line_start >= 0:
-            if start >= 0 and start < line_start:
-                self.is_multiple = True
-                start += len(self.token[0])
-            else:
-                self.is_multiple = False
-                start = line_start + len(self.line_token)
-        elif start >= 0:
-            self.is_multiple = True
-            start += len(self.token[0])
-        return start
+        for token in self.tokens:
+            left_token = token if type(token) is str else token[0]
+            start = line.find(left_token)
+            if start >= 0:
+                self.picked_token = token
+                start += len(left_token)
+                return start
+        return -1
 
     def scan_line_end(self, line):
-        return line.find(self.token[1]) 
+        if type(self.picked_token) is str:
+            return len(line)
+        return line.find(self.picked_token[1])
 
     def scan_line(self, line, line_num):
         start = 0
@@ -57,10 +53,6 @@ class CommentScanner(object):
             if start >= 0:
                 content = content[start:]
                 self.picked_line = line_num
-                if not self.is_multiple:
-                    self.buffer.append(content)
-                    self.print_line_log(line, line_num)
-                    return self.dump()
             else:
                 self.print_line_log(line, line_num)
                 return None
@@ -81,23 +73,23 @@ class CommentScanner(object):
                 comments.append(comment)
         comment = self.dump(len(lines) - 1)
         if comment:
-                comments.append(comment)
+            comments.append(comment)
         return comments
 
 class CommentFetcher(object):
     scanner_params = {
-        'c': [('/*', '*/'), '//'],
-        'cpp': [('/*', '*/'), '//'],
-        'javascript': [('/*', '*/'), '//'],
-        'python': [('\'\'\'', '\'\'\''), '#']
+        'c': ['//', ('/*', '*/')],
+        'cpp': ['//', ('/*', '*/')],
+        'javascript': ['//', ('/*', '*/')],
+        'python': ['#', ('\'\'\'', '\'\'\''), ('\"\"\"', '\"\"\"')]
     }
 
     def __init__(self, language, debug=False):
         params = self.scanner_params.get(language)
         if params:
-            self.scanner = CommentScanner(params[0], params[1], debug)
+            self.scanner = CommentScanner(params, debug)
 
     def fetch(self, content):
         return self.scanner.scan(content)
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
